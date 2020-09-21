7f543e7ff175e44aa1f8ad5f20b4a3bb77518851
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-21 05:00:21 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,67 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let counter = document.getElementById("counter");
-    setInterval(countFun, 1000);
+    let counter = document.getElementById("counter"),
+        minus = document.getElementById("minus"),
+        plus = document.getElementById("plus"),
+        like = document.getElementById("heart"),
+        pause = document.getElementById("pause");
 
-    function countFun() {
-        counter.innerHTML = parseInt(counter.innerHTML) +1;
+    let numbersArray = [];    
+
+    let interval = setInterval(countUp, 1000);
+     
+    function countUp() {
+        counter.innerHTML = parseInt(counter.innerHTML) + 1;
     }
 
+    function countDown() {
+        counter.innerHTML = parseInt(counter.innerHTML) - 1;
+    }
 
+    minus.addEventListener("click", () => {
+        clearInterval(interval);
+        countDown();
+     });
+
+    plus.addEventListener("click", () => {
+        clearInterval(interval);
+        countUp();
+     });
+
+
+     function pasueFun() {
+        if (pause.innerHTML = "pause") {
+            pause.addEventListener("click", () => {
+                pause.innerHTML = "resume";
+                clearInterval(interval);
+            });
+        } 
+          
+
+                
+                    pause.addEventListener("click", () => {
+                        if (pause.innerHTML = "resume") {
+                            clearInterval(interval);
+                            setInterval(countUp, 1000); 
+                            pause.innerHTML = "pause";
+
+                        }
+                    });
+                        
+       
+     }
+    
+
+    // like.addEventListener("click", () => {
+    //     let x = 0;
+    //     x++;
+
+
+    //     let ul = document.getElementById("numOfLikes");
+    //     let li = document.createElement("li");
+    //     li.innerHTML = `${counter.innerHTML} has ${} likes`;
+    //     ul.appendChild(li);
+
+    // });
 
 
 
@@ -29,3 +84,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 });
+
+
+
+
+
